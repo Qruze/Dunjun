75fd6e3784f22f1013bf377d98f00fa47bc4e5ab
Qruze <rasmussen.peter@gmail.com>
2015-04-02 16:35:10 +0200

    Ginger Games - Dunjun - 002

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+Dunjun - Read Me
+================
+
+This is my attempt to follow along [GingerGames](https://github.com/gingerBill/Dunjun) as he makes an c++ game from scratch.
+
+I do not own any of this and all I do is follow this series on youtube
+[![Dunjun - Ginger Games](http://img.youtube.com/vi/fRUYl6_5m3o/0.jpg)](http://www.youtube.com/watch?v=fRUYl6_5m3o)
---
+++ src/Main.cpp
@@ -0,0 +1,41 @@
+#include <Dunjun/Common.hpp>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+
+
+int main(int argc, char** argv)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return EXIT_FAILURE;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(854, 480, "Dunjun", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
