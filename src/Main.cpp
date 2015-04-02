2d58d359546e823baefa3ce14c44f3c444e8f29a
Qruze <rasmussen.peter@gmail.com>
2015-04-02 23:11:42 +0200

    Ginger Games - Dunjun - Day 003
    Drawing With OpenGL (GLEW)

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,26 @@
 #include <Dunjun/Common.hpp>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 #include <iostream>
 
 GLOBAL const int g_windowWidth = 854;
 GLOBAL const int g_windowHeight = 480;
 
+void glfwHints()
+{
+	glfwWindowHint(GLFW_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_VERSION_MINOR, 1);
+}
+
+
 int main(int argc, char** argv)
 {
+	if (!glewInit())
+		return EXIT_FAILURE;
+
+
 	GLFWwindow* window;
 
 	/* Initialize the library */
@@ -15,6 +28,7 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 
 	/* Create a windowed mode window and its OpenGL context */
+	glfwHints();
 	window = glfwCreateWindow(
 	    g_windowWidth, g_windowHeight, "Dunjun", nullptr, nullptr);
 	if (!window)
@@ -26,6 +40,56 @@ int main(int argc, char** argv)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	glewInit();
+
+	float vertices[] = {
+		+0.0f, +0.5f, //Vertex 1
+		-0.5f, -0.5f, //Vertex 2
+		+0.5f, -0.5f, //Vertex 3
+	};
+
+	GLuint vbo; // Vertex Buffer Object
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char* vertexShaderText = {
+		"#version 120\n"
+		"\n"
+		"attribute vec2 position;"
+		"void main()"
+		"{"
+		"	gl_Position = vec4(position, 0.0, 1.0);"
+		"}"
+	};
+
+	const char* fragmentShaderText = {
+		"#version 120\n"
+		"\n"
+		"void main()"
+		"{"
+		"	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+		"}"
+	};
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderText, nullptr);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderText, nullptr);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glBindAttribLocation(shaderProgram, 0, "position");
+
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
 	bool running = true;
 	bool fullscreen = false;
 	/* Loop until the user closes the window */
@@ -34,6 +98,18 @@ int main(int argc, char** argv)
 		/* Render here */
 		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Draw Things*/
+		{
+			glEnableVertexAttribArray(0);
+
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			glDisableVertexAttribArray(0);
+		}
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
@@ -44,13 +120,13 @@ int main(int argc, char** argv)
 		    glfwGetKey(window, GLFW_KEY_ESCAPE))
 			running = false;
 
-		if (glfwGetKey(window, GLFW_KEY_F11))
+		/*if (glfwGetKey(window, GLFW_KEY_F11))
 		{
 			fullscreen = !fullscreen;
 
 			GLFWwindow* newWindow;
-
-			if (fullscreen)
+			glfwHints();
+			if (fullscreen) 
 			{
 				int count;
 				const GLFWvidmode* modes =
@@ -71,7 +147,7 @@ int main(int argc, char** argv)
 			glfwDestroyWindow(window);
 			window = newWindow;
 			glfwMakeContextCurrent(window);
-		}
+		}*/
 	}
 
 	glfwDestroyWindow(window);
